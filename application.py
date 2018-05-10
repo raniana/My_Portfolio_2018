8b4cb1974c720d2a9c0c40adbc2dbf3d5953f2fb
rania idris <rania_idris@outlook.com>
2018-05-10 14:01:05 -0500

    feat: portfolio works on localhost

--- application.py
+++ application.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
 @app.route('/')
 def showMainPage():
 	return render_template('welcome.html')
+
+
+
+
+if __name__ == '__main__':
+
+    
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
+
+
